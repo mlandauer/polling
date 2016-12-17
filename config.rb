2c3b668ca48d6fbd5cdfc10e0082c972001ce0b7
Matthew Landauer <mlandauer@gmail.com>
2016-12-17 18:05:17 +1100

    Show on main poll index page when there isn't enough data to show the results

--- config.rb
+++ config.rb
@@ -16,6 +16,13 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+[false, true].each do |enough|
+  proxy "/polls/1/enough/#{enough}.html",
+        '/polls/1/index.html',
+        locals: { enough: enough },
+        ignore: true
+end
+
 (1..5).each do |no|
   next_url = no == 5 ? '/polls/1/intro_demographic.html' : "#{no+1}.html"
   proxy "/polls/1/questions/#{no}.html",
